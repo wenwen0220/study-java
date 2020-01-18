9585e072f5dbd8bf948003795567cce58c3f2a1b
root <jeffjiangwenwen_v@didiglobal.com>
2020-01-18 15:52:32 +0800

    init

---
+++ java-thread/src/test/java/com/personal/jw/java/JavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.jw.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
